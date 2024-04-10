fc68452f9661e06b22230eddc7c6d27e4020cb29
Maskeit <miguelagustin182@gmail.com>
2024-04-09 22:34:53 -0600

    projects terminated

--- src/components/layouts/Project.tsx
+++ src/components/layouts/Project.tsx
@@ -2,14 +2,32 @@ import './styles/Project.css'
 import { Image, Thumbnail } from '../atomic/Images'
 import { Parragraph, Titles } from '../atomic/Titles';
 import { Projects } from '../../types/cv';
+import { useState } from 'react';
+
 
 export const Project = ({name, description, url, src, thumb } : Projects) => {
-  
+  const [isTouched, setIsTouched] = useState(false);
+
+  const handleTouch = () => {
+    setIsTouched(!isTouched); // Cambia el estado a cada clic/touch
+  };
   return (
-    <div className='project-content'>
-        <Image src={thumb} addClass="grayScale100"/>
-        <Titles text={name} color='main-color' animation='' fontSize=''/>
-        <Parragraph text={description} color='main-color' animation='' fontSize=''/>
+    <div 
+    className={`project-content ${isTouched ? 'touched' : ''}`} 
+    onClick={handleTouch} // Maneja clic/touch aquí
+  >
+
+    <div className='project-description'>
+      <div className="project-title">
+          <Titles text={name} color='' animation='' fontSize=''/>
+      </div>
+      <div className="project-paragraph">
+          <Parragraph text={description} color='' animation='' fontSize=''/>
+      </div>
     </div>
+    <Image src={thumb} addClass=""/>
+
+  </div>
+
   )
 }
--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -2,20 +2,30 @@ import './styles/Projects.css'
 import cv from '../../../cv.json'
 import { Titles } from '../atomic/Titles';
 import { Project } from '../layouts/Project';
+import { Pointer, Blend} from 'lucide-react';
 
 
 export const Proyectos = () => {
   return (
     <div className='projects'>
       <div className='titles-work'>
-        <Titles text='My Professional Journey' fontSize='fs-four-mid' color='main-color' animation='text-reveal'/>
+        <Titles text='My Professional Journey' fontSize='' color='main-color' animation='text-reveal'/>
       </div>
+      <div className='pointer icon-container'>
+        <Pointer />
+        <p>Click on project</p>
+      </div>
+      <div className='hover-me icon-container'>
+        <Blend />
+        <p>Hover on project</p>
+      </div>
+
       <div className='content'>
           {
             cv.projects.map((projectItem, index) => (
               <Project key={index} {...projectItem}/>
               ))
-          }
+          }          
       </div>
     </div>
   );
